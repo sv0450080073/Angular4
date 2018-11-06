971867476c7d4ff3cce40e1a0f0d60ee0b32837c
sv0450080073 <manhkhacnguyen1997@gmail.com>
2018-11-06 10:02:24 +0700

    end lesson32:ng-content

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { PersonComponent } from './person/person.component';
 import { ListPersonComponent } from './list-person/list-person.component';
 import  {ParentComponent } from './parent.component';
 import {ChildComponent }from './child.component';
+import {CardComponent} from './card.component';
 
 @NgModule({
   declarations: [ 
@@ -25,7 +26,8 @@ import {ChildComponent }from './child.component';
     PersonComponent,
     ListPersonComponent,
     ParentComponent,
-    ChildComponent
+    ChildComponent,
+    CardComponent
 
     
   ],
